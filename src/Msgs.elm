85e508495b1f10f583f4aa3549d2aa5e5e049274
Rakesh Mistry <rakesh@qualica.com>
2018-02-14 21:43:46 +0200

    Tutorial completed

--- src/Msgs.elm
+++ src/Msgs.elm
@@ -3,8 +3,11 @@ module Msgs exposing (..)
 import Models exposing (Player)
 import RemoteData exposing (WebData)
 import Navigation exposing (Location)
+import Http
 
 
 type Msg
     = OnFetchPlayers (WebData (List Player))
     | OnLocationChange Location
+    | ChangeLevel Player Int
+    | OnPlayerSave (Result Http.Error Player)
